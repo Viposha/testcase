f772d4e965aaaa349b0431e8f11412c916d017b9
Vitalii Hoshchenko <Viposha@gmail.com>
2024-04-14 18:00:22 +0300

    half way of find_page func

--- tasks/task.py
+++ tasks/task.py
@@ -25,18 +25,20 @@ class Pagination:
             self.pagin[-1] =  self.item_count() % self.items_on_page
             try:
                 return self.pagin[page_number]
-            except IndexError as eror:
-                return ("Invalid index. Page is missing")
+            except IndexError:
+                return ("Exception: Invalid index. Page is missing.")
 
 
-    def find_page(self, data):
-        pass
-    
+    def find_page(self, search_data):
+        split_data = [self.data[i:i+self.items_on_page] for i in range(0, len(self.data), self.items_on_page)]
+        if search_data in split_data:
+            return split_data.index(search_data)
     def display_page(self, page_number):
         pass
 
-pages = Pagination('Your beautiful text111', 3)
+pages = Pagination('Your beautiful text', 5)
 print(pages.page_count())
 print(pages.item_count())
 print(pages.count_items_on_page(0))
 print(pages.count_items_on_page(8))
+print(pages.find_page('Your '))
